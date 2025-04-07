212c9e68e7e8ae12c712015b69d1fe05cc9e02ef
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-04-07 19:26:20 +0530

    feat(App.tsx): Add routes for community pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router";
 import { Home } from "./pages/Home";
 import { Navbar } from "./components/Navbar";
 import { CreatePostPage } from "./pages/CreatePostPage";
+import { PostPage } from "./pages/PostPage";
+import { CreateCommunityPage } from "./pages/CreateCommunityPage";
+import { CommunitiesPage } from "./pages/CommunitiesPage";
+import { CommunityPage } from "./pages/CommunityPage";
 
 function App() {
   return (
@@ -11,6 +15,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/create" element={<CreatePostPage />}></Route>
+          <Route path="/post/:id" element={<PostPage />}></Route>
+          <Route
+            path="/community/create"
+            element={<CreateCommunityPage />}
+          ></Route>
+          <Route path="/communities" element={<CommunitiesPage />} />
+          <Route path="/community/:id" element={<CommunityPage />} />
         </Routes>
       </div>
     </div>
